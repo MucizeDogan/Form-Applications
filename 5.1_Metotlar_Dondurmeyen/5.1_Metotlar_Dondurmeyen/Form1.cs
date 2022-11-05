8ab99916c5a4c9080933842f1623696530b64fff
M-racle <108946729+M-racle@users.noreply.github.com>
2022-11-05 18:02:13 +0300

    I added a simple example using method

---
+++ 5.1_Metotlar_Dondurmeyen/5.1_Metotlar_Dondurmeyen/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5._1_Metotlar_Dondurmeyen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void temizle()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox1.Focus();
+        }
+        private void renklendir()
+        {
+            textBox1.BackColor = Color.YellowGreen;
+            textBox2.BackColor = Color.SaddleBrown;
+            textBox3.BackColor = Color.Chocolate;
+            textBox4.BackColor = Color.Gainsboro;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            temizle();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            renklendir();
+        }
+    }
+}
