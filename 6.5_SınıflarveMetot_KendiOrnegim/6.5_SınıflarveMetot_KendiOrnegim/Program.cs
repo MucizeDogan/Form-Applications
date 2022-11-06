654449664bd39c2b5bca2c111459cf9a95fb5f6d
M-racle <108946729+M-racle@users.noreply.github.com>
2022-11-06 19:32:54 +0300

    Simple implementation using classes and method in console application

---
+++ 6.5_SınıflarveMetot_KendiOrnegim/6.5_SınıflarveMetot_KendiOrnegim/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _6._5_SınıflarveMetot_KendiOrnegim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bilgi blg = new bilgi();
+            string adı,soyadı, yası;
+            Console.Write("Adınızı giriniz: ");
+            adı=Console.ReadLine();
+            Console.Write("Soyadınızı giriniz: ");
+            soyadı=Console.ReadLine();
+            Console.Write("Yaşınızı giriniz: ");
+            yası = Console.ReadLine();
+            blg.isim(adı, soyadı, yası);
+            Console.Read();
+
+
+
+
+        }
+    }
+}
---
+++ 6.5_SınıflarveMetot_KendiOrnegim/6.5_SınıflarveMetot_KendiOrnegim/bilgi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._5_SınıflarveMetot_KendiOrnegim
+{
+    internal class bilgi
+    {
+        public void isim(string ad, string soyad,string yas)
+        {
+            Console.WriteLine("Ad: " + ad);
+            Console.WriteLine("Soyad: "+soyad);
+            Console.WriteLine("Yaş: " + yas);
+
+        }
+    }
+}
