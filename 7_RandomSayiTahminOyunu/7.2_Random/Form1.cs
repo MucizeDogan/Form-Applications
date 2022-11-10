f5f2a9a8366f395e3e1cd330243bd3d707cc5791
M-racle <108946729+M-racle@users.noreply.github.com>
2022-11-10 18:27:47 +0300

    A 4-digit word guessing game that we created using Random.
    The user enters 4 numbers. Those that match the numbers generated by Random are colored green and those that do not match are colored red.

---
+++ 7_RandomSayiTahminOyunu/7.2_Random/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7._2_Random
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int sayi;
+            sayi = rnd.Next(1,10);
+            label1.Text = sayi.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            int s1, s2, s3, s4;
+            s1 = rd.Next(1,5);
+            s2 = rd.Next(1,5);
+            s3 = rd.Next(1, 5);
+            s4 = rd.Next(1, 5);
+            label2.Text = s1.ToString();
+            label3.Text = s2.ToString();
+            label4.Text = s3.ToString();
+            label5.Text = s4.ToString();
+
+            
+
+           if (label2.Text==textBox1.Text)
+            {
+                textBox1.BackColor = Color.Green;
+            }
+           else
+            {
+                textBox1.BackColor = Color.Red;
+            }
+            if (label3.Text == textBox2.Text)
+            {
+                textBox2.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox2.BackColor = Color.Red;
+            }
+            if (label4.Text == textBox3.Text)
+            {
+                textBox3.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox3.BackColor = Color.Red;
+            }
+            if (label5.Text == textBox4.Text)
+            {
+                textBox4.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox4.BackColor = Color.Red;
+            }
+
+
+
+
+
+        }
+    }
+}
