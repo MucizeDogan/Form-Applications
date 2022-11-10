390370ca828c31c5e535b5879c4c1ee7d9968aa6
M-racle <108946729+M-racle@users.noreply.github.com>
2022-11-10 18:41:39 +0300

    Creating a text document

---
+++ 8.3_MetinBelgesiOlusturma/8.3_MetinBelgesiOlusturma/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace _8._3_MetinBelgesiOlusturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string dosyaAdi, dosyaYolu;
+        StreamWriter sw;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dosyaAdi = textBox2.Text;
+            sw = File.CreateText(dosyaAdi+".txt");
+            sw.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                dosyaYolu = folderBrowserDialog1.SelectedPath.ToString();
+                textBox1.Text = dosyaYolu;
+
+            }
+        }
+    }
+}
